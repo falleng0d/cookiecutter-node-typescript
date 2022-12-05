8e3a23290f72d68942be6aaab4114be7d4014d70
falleng0d <juniormateusknd@gmail.com>
2022-12-04 21:34:19 -0300

    Upd post gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,5 +3,3 @@ import subprocess
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', '{{cookiecutter.repo_name}} init'])
-
-subprocess.call(['yarn', 'install'])
